42ce5ee87132a8b8b11e56a5421ef4d3d95eed06
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-05-17 16:08:42 +0900

    Initial Commit

---
+++ Diary+NewWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary+NewWord
+//
+//  Created by 조규연 on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
