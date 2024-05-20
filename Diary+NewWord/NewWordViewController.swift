9c035e71cd2ee1a2a2464e3aff8e93e2bacd93a0
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-05-20 17:21:33 +0900

    오토레이아웃 적용

---
+++ Diary+NewWord/NewWordViewController.swift
@@ -0,0 +1,132 @@
+//
+//  NewWordViewController.swift
+//  Diary+NewWord
+//
+//  Created by 조규연 on 5/17/24.
+//
+
+import UIKit
+
+class NewWordViewController: UIViewController {
+
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+    @IBOutlet var firstKeywordButton: UIButton!
+    @IBOutlet var secondKeywordButton: UIButton!
+    @IBOutlet var thirdKeywordButton: UIButton!
+    @IBOutlet var fourthKeywordButton: UIButton!
+    @IBOutlet var resultImageView: UIImageView!
+    @IBOutlet var resultLabel: UILabel!
+    
+    // Array
+    let newWords = ["분좋카","완내스","그잡채","H워얼V","700","빠태"]
+    let newWordsExplains = ["'분위기 좋은 카페'의 줄임말입니다.","'완전 내 스타일'의 줄임말입니다.","'그 자체'를 소리나는 대로 적은 것입니다.","단어를 위아래로 뒤집어보면 '사랑해'가 됩니다.","'귀여워'에서 자음만 쓰면 ㄱㅇㅇ입니다. 이를 숫자로 바꿔서 700이라고 씁니다.","'빠른 태세 전환'의 줄임말입니다."]
+    
+    // Dictionary
+    let newWordDic = [
+        "분좋카":"'분위기 좋은 카페'의 줄임말입니다.",
+        "완내스":"'완전 내 스타일'의 줄임말입니다.",
+        "그잡채":"'그 자체'를 소리나는 대로 적은 것입니다.",
+        "H워얼V":"단어를 위아래로 뒤집어보면 '사랑해'가 됩니다.",
+        "700":"'귀여워'에서 자음만 쓰면 ㄱㅇㅇ입니다. 이를 숫자로 바꿔서 700이라고 씁니다.",
+        "빠태":"'빠른 태세 전환'의 줄임말입니다."]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        searchTextField.leftViewMode = .always
+        searchTextField.placeholder = "신조어를 입력해주세요."
+        searchTextField.layer.borderColor = UIColor.black.cgColor
+        searchTextField.layer.borderWidth = 2
+        
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.setTitle("", for: .normal)
+        searchButton.backgroundColor = .black
+        searchButton.tintColor = .white
+        
+        let buttons = [firstKeywordButton, secondKeywordButton, thirdKeywordButton, fourthKeywordButton]
+        
+        buttons.forEach {
+            $0?.titleLabel?.font = .systemFont(ofSize: 12)
+            $0?.layer.borderColor = UIColor.black.cgColor
+            $0?.layer.borderWidth = 1
+            $0?.layer.cornerRadius = 5
+        }
+        
+        showRandomCategory()
+        
+        resultImageView.image = .background
+        resultImageView.contentMode = .scaleAspectFill
+        
+        resultLabel.text = ""
+        resultLabel.textAlignment = .center
+        resultLabel.numberOfLines = 0
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        search()
+    }
+    
+    @IBAction func returnTapped(_ sender: UITextField) {
+        search()
+    }
+    
+    @IBAction func firstKeywordButtonTapped(_ sender: UIButton) {
+        let buttons = [firstKeywordButton,secondKeywordButton,thirdKeywordButton,fourthKeywordButton]
+        keywordButtonTapped(button: buttons[sender.tag]!)
+    }
+    
+    func keywordButtonTapped(button: UIButton) {
+        searchTextField.text = button.currentTitle
+        search()
+        showRandomCategory()
+    }
+    
+    func showRandomCategory() {
+        let buttons = [firstKeywordButton,secondKeywordButton,thirdKeywordButton,fourthKeywordButton]
+        
+        //Array
+//        let shuffledNewWords = newWords.shuffled()
+        
+        //Dictionary
+        let shuffledNewWords = Array(newWordDic.keys).shuffled()
+        
+        
+        for (index, element) in buttons.enumerated() {
+            element?.setTitle("\(shuffledNewWords[index])", for: .normal)
+        }
+    }
+    
+    func search() {
+        //Array
+//        switch searchTextField.text {
+//        case newWords[0]:
+//            resultLabel.text = newWordsExplains[0]
+//        case newWords[1]:
+//            resultLabel.text = newWordsExplains[1]
+//        case newWords[2]:
+//            resultLabel.text = newWordsExplains[2]
+//        case newWords[3]:
+//            resultLabel.text = newWordsExplains[3]
+//        case newWords[4]:
+//            resultLabel.text = newWordsExplains[4]
+//        case newWords[5]:
+//            resultLabel.text = newWordsExplains[5]
+//        default:
+//            resultLabel.text = "검색결과가 없습니다."
+//        }
+        
+        //Dictionary
+        if let searchText = searchTextField.text, let result = newWordDic[searchText] {
+            resultLabel.text = result
+        } else {
+            resultLabel.text = "검색결과가 없습니다."
+        }
+    }
+}
--- Diary+NewWord/ViewController.swift
+++ Diary+NewWord/ViewController.swift
@@ -9,11 +9,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstButton: UIButton!
+    @IBOutlet var secondButton: UIButton!
+    @IBOutlet var thirdButton: UIButton!
+    @IBOutlet var fourthButton: UIButton!
+    @IBOutlet var fifthButton: UIButton!
+    @IBOutlet var sixthButton: UIButton!
+    @IBOutlet var seventhButton: UIButton!
+    @IBOutlet var eighthButton: UIButton!
+    @IBOutlet var ninethButton: UIButton!
+    
+    // Array
+    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let labels = ["행복해","사랑해","좋아해","당황해","속상해","우울해","심심해","원통해","비참해"]
+    
+    // Dictionary
+    var buttonDic: [UIButton:(label: String, count: Int)] = [:]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let buttons = [firstButton, secondButton, thirdButton, fourthButton, fifthButton, sixthButton, seventhButton, eighthButton, ninethButton]
+        
+        for (index, button) in buttons.enumerated() {
+            button?.configuration?.imagePlacement = .top
+            button?.configuration?.imagePadding = 8
+            
+            // Array
+//            button?.setImage(configureImage(name: "slime\(index + 1)"), for: .normal)
+//            button?.setTitle("\(labels[index]) 0", for: .normal)
+            
+            // Dictionary
+            if let button {
+                buttonDic[button] = (labels[index], 0)
+                button.setImage(configureImage(name: "slime\(index + 1)"), for: .normal)
+                button.setTitle("\(labels[index]) 0", for: .normal)
+            }
+        }
+    }
+    
+    func configureImage(name: String) -> UIImage? {
+        let image = UIImage(named: name)?.resize(targetSize: CGSize(width: 100, height: 100))?.withRenderingMode(.alwaysOriginal)
+        
+        return image
     }
 
-
+    @IBAction func firstButtonTapped(_ sender: UIButton) {
+        let buttons = [firstButton, secondButton, thirdButton, fourthButton, fifthButton, sixthButton, seventhButton, eighthButton, ninethButton]
+        buttonTapped(button: buttons[sender.tag]!)
+    }
+    
+    // Array
+//    func buttonTapped(index: Int, button: UIButton) {
+//        counts[index] += 1
+//        button.setTitle("\(labels[index]) \(counts[index])", for: .normal)
+//    }
+    
+    // Dictionary
+    func buttonTapped(button: UIButton) {
+        if var data = buttonDic[button] {
+            data.count += 1
+            buttonDic[button] = data
+            button.setTitle("\(data.label) \(data.count)", for: .normal)
+        }
+    }
 }
 
+extension UIImage {
+    func resize(targetSize: CGSize) -> UIImage? {
+        let newRect = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newRect.size, false, 0)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        context.interpolationQuality = .high
+        draw(in: newRect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
