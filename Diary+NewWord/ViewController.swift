94427b2f3a101acf01aff94ccd4834ce06bf0222
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-05-22 17:14:43 +0900

    Diary에 UserDefault 적용

--- Diary+NewWord/ViewController.swift
+++ Diary+NewWord/ViewController.swift
@@ -20,12 +20,20 @@ class ViewController: UIViewController {
     @IBOutlet var ninethButton: UIButton!
     
     // Array
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+//    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     let labels = ["행복해","사랑해","좋아해","당황해","속상해","우울해","심심해","원통해","비참해"]
     
     // Dictionary
     var buttonDic: [UIButton:(label: String, count: Int)] = [:]
     
+    var counts: [Int] {
+        get {
+            UserDefaults.standard.array(forKey: "counts") as? [Int] ?? [0,0,0,0,0,0,0,0,0]
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "counts")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,9 +45,9 @@ class ViewController: UIViewController {
             if let button {
                 button.configuration?.imagePlacement = .top
                 button.configuration?.imagePadding = 8
-                buttonDic[button] = (labels[index], 0)
+                buttonDic[button] = (labels[index], counts[index])
                 button.setImage(configureImage(name: "slime\(index + 1)"), for: .normal)
-                button.setTitle("\(labels[index]) 0", for: .normal)
+                button.setTitle("\(labels[index]) \(counts[index])", for: .normal)
             }
         }
     }
@@ -52,17 +60,22 @@ class ViewController: UIViewController {
 
     @IBAction func firstButtonTapped(_ sender: UIButton) {
         let buttons = [firstButton, secondButton, thirdButton, fourthButton, fifthButton, sixthButton, seventhButton, eighthButton, ninethButton]
-        buttonTapped(button: buttons[sender.tag]!)
+        if var data = buttonDic[sender] {
+            data.count += 1
+            buttonDic[sender] = data
+            sender.setTitle("\(data.label) \(data.count)", for: .normal)
+            counts[sender.tag] = data.count
+        }
     }
     
     // Dictionary
-    func buttonTapped(button: UIButton) {
-        if var data = buttonDic[button] {
-            data.count += 1
-            buttonDic[button] = data
-            button.setTitle("\(data.label) \(data.count)", for: .normal)
-        }
-    }
+//    func buttonTapped(button: UIButton) {
+//        if var data = buttonDic[button] {
+//            data.count += 1
+//            buttonDic[button] = data
+//            button.setTitle("\(data.label) \(data.count)", for: .normal)
+//        }
+//    }
 }
 
 extension UIImage {
